0504f6c1e613767c7fc000b82f0b0f43cace0dc5
Anastasiq Peychev  KSI 37b <116686740+anastasiqpeychevksi37b@users.noreply.github.com>
2022-12-21 15:43:19 +0200

    Add files via upload

---
+++ character.py
@@ -0,0 +1,6 @@
+class Geroi:
+    def__init__(self,name,egn)
+    self.name
+    self.egn
+G=Geroi("Anastasija",123222002)
+print(G)
---
+++ main.py
@@ -0,0 +1,10 @@
+choice=None
+chars={}
+POOL=30
+while choice !="0":
+    print("Character Creator Please choose your option below 1.Create a new character 2.Create a new weapon for a character  3.Load all characters 4.Delete a character")
+    choice=input("Choice:")
+    while(x!=0):
+     if choice=="0":
+        print("Bye.")
+   
